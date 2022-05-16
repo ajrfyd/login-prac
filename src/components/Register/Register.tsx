5ac5ae6dd17b059aa9cf0144f470d4572de6a0f0
ajrfyd <ajrfyd@naver.com>
2022-05-16 18:31:28 +0900

    Install: dep install

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from 'styled-components';
 import CustomInput from "../utils/Input";
+import { user_regex, pass_regex } from "../../utils/utils";
+
 
 const Register = () => {
   const submitHandler = () => {
     
   }
-
+  const userRef = useRef();
+  const errRef = useRef();
 
   return (
     <Section>
---
+++ src/utils/utils.js
@@ -0,0 +1,5 @@
+export const utils = {
+}
+
+export const user_regex = /^[a-zA-Z][a-zA-Z0-9-_]{3, 23}$/;
+export const pass_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8, 24}$/;
